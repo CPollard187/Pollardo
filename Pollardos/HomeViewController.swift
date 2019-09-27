0643dc688d496cb8b9096679696cc4365df8ff7d
Cody Pollard <codypollard01@stclairconnect.ca>
2019-09-26 22:25:40 -0400

    Added Content to my HomeScreen. Also added the Controllers for my cart and menu.

--- Pollardos/HomeViewController.swift
+++ Pollardos/HomeViewController.swift
@@ -8,8 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
+    //MARK: OUTLETS
+    @IBOutlet weak var pollardoTitle: UILabel!
+    @IBOutlet weak var companyInfo: UILabel!
+    @IBOutlet weak var deals: UILabel!
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var dealImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
